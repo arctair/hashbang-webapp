829fce8776650e17b3343b9a8e923c7a28fcdcd4
tyler <tyler@cruftbusters.com>
2020-11-14 16:25:50 -0600

    style new named tag list

--- src/NewNamedTagList.js
+++ src/NewNamedTagList.js
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import { createNamedTagList } from './hashbang-api'
+import './NamedTagList.css'
 
 function NewNamedTagList() {
   const [name, setName] = useState('')
   const [tags, setTags] = useState([])
   return (
-    <div data-testid="newNamedTagList">
+    <div className="namedTagList" data-testid="newNamedTagList">
       <input
+        className="name"
         data-testid="name"
         onChange={(e) => setName(e.target.value)}
       />
       <input
+        className="tags"
         data-testid="tags"
         onChange={(e) => setTags(e.target.value.split(' '))}
       />
       <button
+        className="create"
         data-testid="create"
         onClick={() => createNamedTagList({ name, tags })}
       />
