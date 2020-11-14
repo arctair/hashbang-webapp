a359361875a1d706466de5d36466ac2756e191a6
tyler <tyler@cruftbusters.com>
2020-11-14 17:34:10 -0600

    hide id

--- src/NamedTagList.js
+++ src/NamedTagList.js
@@ -3,7 +3,9 @@ import './NamedTagList.css'
 function NamedTagList({ id, name, tags }) {
   return (
     <div className="namedTagList" data-testid="namedTagList">
-      <div data-testid="id">{id}</div>
+      <div className="id" data-testid="id">
+        {id}
+      </div>
       <div className="name" data-testid="name">
         {name}
       </div>
