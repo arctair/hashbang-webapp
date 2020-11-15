be627f25bc04507b04c514b1cff5934c77bd155f
arctair <jtyler_@outlook.com>
2020-11-15 07:31:49 -0600

    add new named tag list placeholders

--- src/NewNamedTagList.js
+++ src/NewNamedTagList.js
@@ -10,11 +10,13 @@ function NewNamedTagList() {
       <input
         className="name"
         data-testid="name"
+        placeholder="autumn"
         onChange={(e) => setName(e.target.value)}
       />
       <textarea
         className="tags"
         data-testid="tags"
+        placeholder="#craftbeer #leaves"
         onChange={(e) => setTags(e.target.value.split(' '))}
       />
       <button
